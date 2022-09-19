cacfb8177c351292dc4e02d5b83d2a2917be5d96
Felandim <62677718+Felandim@users.noreply.github.com>
2022-09-18 23:37:29 -0300

    bunch of stuff.

--- Udemy - Go The Complete Developer's Guide (Golang)/structs/main.go
+++ Udemy - Go The Complete Developer's Guide (Golang)/structs/main.go
@@ -2,17 +2,36 @@ package main
 
 import "fmt"
 
+type contactInfo struct {
+	email   string
+	zipCode int
+}
+
 type person struct {
 	firstName string
 	lastName  string
+	contactInfo
 }
 
 func main() {
-	var alex person
+	jim := person{
+		firstName: "Jim",
+		lastName:  "Halpert",
+		contactInfo: contactInfo{
+			email:   "jim.halpert@DunderMifflin.com",
+			zipCode: 94000,
+		},
+	}
 
-	alex.firstName = "Alex"
-	alex.lastName = "Anderson"
+	jim.updateName("Jimmy")
+	jim.print()
+}
 
-	fmt.Println(alex)
-	fmt.Printf("%+v", alex)
+func (p person) print() {
+	fmt.Printf("%+v", p)
+
+}
+
+func (pointerToPerson *person) updateName(newFirstName string) {
+	(*pointerToPerson).firstName = newFirstName
 }
